85cdf561825418626164b6722e6be112a4ca25aa
KARAN'S MAC <karannarode@CodeZ-MacBook-Air.local>
2024-12-29 23:28:31 +0530

    final commit

--- docs/src/components/Foreground.jsx
+++ docs/src/components/Foreground.jsx
@@ -18,6 +18,14 @@ function Foreground() {
             tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
 
         },
+        {
+            desc: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+            fileSize: ".9mb",
+            close: false,
+            tag: { isOpen: false, tagTitle: "Download Now", tagColor: "green" },
+
+        },
+
     ];
     return (
         <div ref={ref} className="fixed top-0 left-0 z-[3] w-full h-full flex gap-10 p-5 flex-wrap">
